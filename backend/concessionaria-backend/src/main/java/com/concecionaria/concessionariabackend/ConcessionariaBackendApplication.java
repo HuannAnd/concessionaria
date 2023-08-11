b2376df405db36e298a7a037658439993366d4ef
Koioto13224 <levyvinicius.alb61@gmail.com>
2023-08-11 19:01:36 -0300

    configuração do backend

---
+++ backend/concessionaria-backend/src/main/java/com/concecionaria/concessionariabackend/ConcessionariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.concecionaria.concessionariabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionariaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/concessionaria-backend/src/test/java/com/concecionaria/concessionariabackend/ConcessionariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concecionaria.concessionariabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
