f91b2f0bba4f2db0d4d7af7d5a996ffb461a20eb
Koioto13224 <levyvinicius.alb61@gmail.com>
2023-10-20 13:58:37 -0300

    alteração dos nomes dos objetos do userService

--- backend/concessionaria-backend/src/main/java/com/concecionaria/concessionariabackend/Services/UserService.java
+++ backend/concessionaria-backend/src/main/java/com/concecionaria/concessionariabackend/Services/UserService.java
@@ -16,12 +16,12 @@ public class UserService {
     public UserService(UserRepository clienteRepository) {
         this.clienteRepository = clienteRepository;}
 
-    public Users novoUsuario(Users usuarios) {
-        usuarios.setRole(UserRoles.USER);
-        return clienteRepository.save(usuarios);
+    public Users newUsers(Users users) {
+        users.setRole(UserRoles.USER);
+        return clienteRepository.save(users);
     }
 
-    public UserDetails buscaUsuarios(String usuario) {
-        return clienteRepository.findByEmail(usuario);
+    public UserDetails findUsers(String users) {
+        return clienteRepository.findByEmail(users);
     }
 }
